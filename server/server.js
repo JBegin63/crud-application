c5383e306bdde6d5782027f8450fdbb2e6d1a4e0
JBegin63 <100534652+JBegin63@users.noreply.github.com>
2022-10-01 03:12:18 -0400

    Mini Project complete

--- server/server.js
+++ server/server.js
@@ -13,16 +13,60 @@ const db = mysql.createPool({
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/get", (req, res) => {
+    const sqlSelect = 
+        "SELECT * FROM movie_reviews";
+
+    db.query(sqlSelect, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+        console.log(result);
+    });
+});
 
 app.post("/api/insert", (req,res) => {
     const movieName = req.body.movieName;
     const movieReview = req.body.movieReview;
-
     const sqlInsert = 
-    "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+        "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+
     db.query(sqlInsert, [movieName, movieReview], (err, result) => {
-        console.log(err);
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
     });
+});
+
+app.delete('/api/delete/:movieName', (req, res) => {
+    const name = req.params.movieName
+    const sqlDelete = 
+        "DELETE FROM movie_reviews WHERE movieName = ?";
+
+    db.query(sqlDelete, name, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+    })
+});
+
+app.put('/api/update', (req, res) => {
+    const name = req.body.movieName;
+    const review = req.body.movieReview;
+    const sqlUpdate = 
+        "UPDATE movie_reviews SET movieReview = ? WHERE movieName = ?";
+
+    db.query(sqlUpdate, [review, name], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+    })
 });
 
 app.listen(3001, () => {
